db9ad7616213da1a5e1c11aee9cc11cbf34f8f7c
pan <panming@itcast.cn>
2018-07-25 15:02:04 +0800

    实现了新闻详情页的上半部分

--- src/views/news/Detail.vue
+++ src/views/news/Detail.vue
@@ -1,5 +1,46 @@
 <template>
-    <div class="page">
-        新闻详情
+    <div class="page news">
+        <div class="newsdetail">
+            <div class="newstitle">
+                <h4>{{newsInfo.title}}</h4>
+                <p class="news-info">
+                    <span>发表时间: {{newsInfo.add_time}}</span>
+                    <span class="mui-pull-right">点击： {{newsInfo.click}}次</span>
+                </p>
+                <hr>
+            </div>
+            <div class="newscontent" v-html="newsInfo.content"></div>
+        </div>
     </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      newsInfo: {}
+    };
+  },
+  created() {
+    // 1. 获取传入的id
+    // alert(this.$route.query.id)
+    // 2. 通过ajax请求获取新闻数据
+    axios({
+      url: "http://www.liulongbin.top:3005/api/getnew/" + this.$route.query.id
+    }).then(res => {
+      // console.log(res.data.message);
+      this.newsInfo = res.data.message[0];
+    });
+  }
+};
+</script>
+
+<style>
+.news{
+    padding: 5px;
+}
+
+.newscontent img{
+    width: 100%;
+}
+</style>
--- src/views/news/List.vue
+++ src/views/news/List.vue
@@ -2,13 +2,13 @@
     <div class="page">
         <ul class="mui-table-view mui-table-view-chevron">
             <li class="mui-table-view-cell mui-media" v-for="(item, index) in newsList" :key="index">
-                <a class="mui-navigate-right">
+                <router-link :to="'/news/detail?id=' + item.id" class="mui-navigate-right">
                     <img class="mui-media-object mui-pull-left" :src="item.img_url">
                     <div class="mui-media-body mui-ellipsis">
                         {{item.title}}
                         <p class="mui-ellipsis">{{item.zhaiyao}}</p>
                     </div>
-                </a>
+                </router-link>
             </li>
         </ul>
     </div>
